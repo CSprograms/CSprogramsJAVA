a82621d1f92a367d44cdfdf558a6290b5c27ab0f
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-25 14:58:39 +0530

    Create program05.java

---
+++ program05.java
@@ -0,0 +1,12 @@
+/**
+String concatenation in println method
+22/02/2022
+*/
+
+public class program05
+{
+   public static void main(String[] args) 
+   {
+      System.out.print("Welcome to " + "Java Programming!");
+   } 
+} 
