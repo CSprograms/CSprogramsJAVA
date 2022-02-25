166eae1967b25a0708dccb6d6bae81218d5582d1
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-25 14:59:06 +0530

    Create program07.java

---
+++ program07.java
@@ -0,0 +1,13 @@
+/**
+String concatenation VS Addition
+22/02/2022
+*/
+
+public class program07
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("String Concatenation: " + 5 + 3);
+	  System.out.print("Addition: " + (5 + 3));
+   } 
+} 
