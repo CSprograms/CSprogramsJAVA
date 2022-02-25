27d93f1b907cbdee98ae55a47d45dfe67f0e6d14
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-25 14:55:50 +0530

    Create program01.java

---
+++ program01.java
@@ -0,0 +1,12 @@
+/**
+Printing a string using println method
+22/02/2022
+*/
+
+public class program01 
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Welcome to Java Programming!");
+   } 
+} 
