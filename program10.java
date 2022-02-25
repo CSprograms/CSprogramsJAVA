f1ad784ad20d5de4b0e34c54865a4f0963c0df12
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-25 15:39:49 +0530

    Create program10.java

---
+++ program10.java
@@ -0,0 +1,13 @@
+/**
+ * Kilometer to Miles
+ */
+public class program10 {
+    public static void main(String[] args){
+        final double MILES_PER_KM = 0.621371;
+		double kiloMeter = 3.2;
+		double miles = kiloMeter * MILES_PER_KM;
+		
+        System.out.println("Kilometer : " + kiloMeter);
+        System.out.print("Miles : " + miles);
+    }
+}
