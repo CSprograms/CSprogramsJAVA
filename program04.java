2846aa1470e97c5f0c7561f6d7363003fa86fbdc
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-25 14:57:12 +0530

    Create program04.java

---
+++ program04.java
@@ -0,0 +1,15 @@
+/**
+println vs print method
+22-02-2022
+*/
+
+public class program04
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Welcome to");
+	  System.out.println(" Java Programming!");
+	  System.out.print("Welcome to");
+	  System.out.print(" Java Programming!");
+   } 
+} 
