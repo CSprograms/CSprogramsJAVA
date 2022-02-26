ecb8b280fc8210adddca76f65e7258cc9225bb00
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-26 23:36:24 +0530

    Update program09.java

--- program09.java
+++ program09.java
@@ -1,13 +1,23 @@
 /**
- * Celsius to Fahrenheit
+ * Arithmetic Operations on integers
  */
+import java.util.Scanner;
 public class program09 {
-    public static void main(String[] args){
-        int c = 28;
-		//Fahrenheit = 32 + ( ( 9 * Celsius ) / 5 )
-        int f = 32 + (( 9 * c)/5);
-
-        System.out.println("Celsius : " + c);
-        System.out.print("Fahrenheit : " + f);
-    }
+  public static void main(String[] args) {
+    Scanner ip = new Scanner(System.in);
+    
+    System.out.print("Enter Input 1 : ");
+    int ip1 = ip.nextInt();
+    
+    System.out.print("Enter Input 2 : ");
+    int ip2 = ip.nextInt();
+    
+    ip.close();
+    
+    System.out.printf("%d + %d = %d%n", ip1, ip2, (ip1+ip2));
+    System.out.printf("%d - %d = %d%n", ip1, ip2, (ip1-ip2));
+    System.out.printf("%d * %d = %d%n", ip1, ip2, (ip1*ip2));
+    System.out.printf("%d / %d = %d%n", ip1, ip2, (ip1/ip2));
+    System.out.printf("%d %s %d = %d%n", ip1, "%", ip2, (ip1%ip2));
+  }
 }
