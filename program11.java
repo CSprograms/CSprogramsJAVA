22a4f32cc604a2c5360be55e02122db59168d299
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-26 23:36:43 +0530

    Update program11.java

--- program11.java
+++ program11.java
@@ -1,12 +1,14 @@
 /**
- * Lowercase to Uppercasea
+ * Celsius to Fahrenheit
  */
 public class program11 {
     public static void main(String[] args){
-        char lC = 'a';
-		char uC = (char)(lC - 32);
+        int c = 28;
+        
+        //Fahrenheit = 32 + ( ( 9 * Celsius ) / 5 )
+        int f = 32 + (( 9 * c)/5);
 
-        System.out.println("Lowercase : " + lC);
-        System.out.print("Uppercase : " + uC);
+        System.out.println("Celsius : " + c);
+        System.out.print("Fahrenheit : " + f);
     }
 }
