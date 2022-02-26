c5e91c8e1617fe893b7f379d3724d10118b3cb3c
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-02-26 23:36:31 +0530

    Update program10.java

--- program10.java
+++ program10.java
@@ -1,13 +1,23 @@
 /**
- * Kilometer to Miles
+ * Arithmetic Operations on double
  */
+import java.util.Scanner;
 public class program10 {
-    public static void main(String[] args){
-        final double MILES_PER_KM = 0.621371;
-		double kiloMeter = 3.2;
-		double miles = kiloMeter * MILES_PER_KM;
-		
-        System.out.println("Kilometer : " + kiloMeter);
-        System.out.print("Miles : " + miles);
-    }
+  public static void main(String[] args) {
+    Scanner ip = new Scanner(System.in);
+    
+    System.out.print("Enter Input 1 : ");
+    double ip1 = ip.nextDouble();
+    
+    System.out.print("Enter Input 2 : ");
+    double ip2 = ip.nextDouble();
+    
+    ip.close();
+    
+    System.out.printf("%.4f + %.4f = %.4f%n", ip1, ip2, (ip1+ip2));
+    System.out.printf("%.4f - %.4f = %.4f%n", ip1, ip2, (ip1-ip2));
+    System.out.printf("%.4f * %.4f = %.4f%n", ip1, ip2, (ip1*ip2));
+    System.out.printf("%.4f / %.4f = %.4f%n", ip1, ip2, (ip1/ip2));
+    System.out.printf("%.4f %s %.4f = %.4f%n", ip1, "%", ip2, (ip1%ip2));
+  }
 }
