10959cbdd1ef9c5a234f760f9f1de4f3590152f4
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-03 11:22:39 +0530

    Update program05.java

--- program05.java
+++ program05.java
@@ -2,10 +2,9 @@
 * String concatenation in println method
 */
 
-public class program05
-{
-   public static void main(String[] args) 
-   {
+public class program05 {
+   public static void main(String[] args) {
+      
       System.out.print("Welcome to " + "Java Programming!");
    } 
 } 
