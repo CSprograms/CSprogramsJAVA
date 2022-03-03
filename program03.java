31782ae38aeb56d7ec20ade88c87c55e00f8c85b
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-03 11:22:16 +0530

    Update program03.java

--- program03.java
+++ program03.java
@@ -2,11 +2,10 @@
 * Printing multi-line string using multiple println method
 */
 
-public class program03
-{
-   public static void main(String[] args) 
-   {
+public class program03 {
+   public static void main(String[] args) {
+      
       System.out.println("Welcome to");
-	  System.out.println(" Java Programming!");
+      System.out.println(" Java Programming!");
    } 
 } 
