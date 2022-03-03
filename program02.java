45065775507766168d3230cf8a9962693c039a69
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-03 11:22:00 +0530

    Update program02.java

--- program02.java
+++ program02.java
@@ -2,10 +2,9 @@
 * Printing multi-line string using println method
 */
 
-public class program02 
-{
-   public static void main(String[] args) 
-   {
+public class program02 {
+   public static void main(String[] args) {
+      
       System.out.println("Welcome to\n Java Programming!");
    } 
 } 
