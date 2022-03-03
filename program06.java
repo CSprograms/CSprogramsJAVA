50ea00bd22797669526eae42dc526c77e1c82fe1
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-03 11:22:47 +0530

    Update program06.java

--- program06.java
+++ program06.java
@@ -2,10 +2,11 @@
 * String formatting in printf method
 */
 
-public class program06
-{
-   public static void main(String[] args) 
-   {
-      System.out.printf("%s%n%s","Welcome to " , "Java Programming!");
+public class program06 {
+   public static void main(String[] args) {
+
+      System.out.printf("%s%n","Welcome to Java Programming!");
+      
+      System.out.printf("%s\n%s","Welcome to " , "Java Programming!");
    } 
 } 
