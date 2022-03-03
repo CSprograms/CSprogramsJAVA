03bbfd4768e7184c9c500809a0bf2b5c57410fb9
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-03 11:22:30 +0530

    Update program04.java

--- program04.java
+++ program04.java
@@ -2,13 +2,13 @@
 * println vs print method
 */
 
-public class program04
-{
-   public static void main(String[] args) 
-   {
+public class program04 {
+   public static void main(String[] args) {
+
       System.out.println("Welcome to");
-	  System.out.println(" Java Programming!");
-	  System.out.print("Welcome to");
-	  System.out.print(" Java Programming!");
+      System.out.println(" Java Programming!");
+      
+      System.out.print("Welcome to");
+      System.out.print(" Java Programming!");
    } 
 } 
