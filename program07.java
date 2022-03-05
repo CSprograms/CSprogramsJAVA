bd58d6c75e55f304d8de93ba317f4c723864f082
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-05 20:38:56 +0530

    Update program07.java

--- program07.java
+++ program07.java
@@ -6,7 +6,9 @@ public class program07
 {
    public static void main(String[] args) 
    {
+
       System.out.println("String Concatenation: " + 5 + 3);
-	  System.out.print("Addition: " + (5 + 3));
+      
+	   System.out.print("Addition: " + (5 + 3));
    } 
 } 
