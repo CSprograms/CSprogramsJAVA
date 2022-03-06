0de89a95cd024ff6e477cf92058a35122396c050
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-06 19:48:12 +0530

    Update program21.java

--- program21.java
+++ program21.java
@@ -1,6 +1,5 @@
 /**
- * Biggest amonng Three Numbers -- Version 1
- * Using if else and logical operator
+ * Swaping -- without temporary variable
  */
 import java.util.Scanner;
 public class program21 {
@@ -12,23 +11,20 @@ public class program21 {
 
         System.out.print("Enter Input 2 : ");
         int input2 = input.nextInt();
-
-        System.out.print("Enter Input 3 : ");
-        int input3 = input.nextInt();
+        
         input.close();
 
-        int biggest;
-        
-        if(input1 >= input2 && input1 >= input3) {
-            biggest = input1;
-        }
-        else if(input2 >= input3) {
-            biggest = input2;
-        }
-        else {
-            biggest = input3;
-        }
+        System.out.println("Before Swap");
+        System.out.println("Input 1 : " + input1);
+        System.out.println("Input 2 : " + input2);
 
-        System.out.println("Biggest Value : " + biggest);
+        input1 = input1 + input2;
+        input2 = input1 - input2;
+        input1 = input1 - input2;
+
+        System.out.println("After Swap");
+        System.out.println("Input 1 : " + input1);
+        System.out.println("Input 2 : " + input2);
+
     }
 }
