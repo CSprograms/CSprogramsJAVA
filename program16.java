e8faacfbb4b47facebb2375003b0ed3df60ea19c
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-06 19:47:40 +0530

    Update program16.java

--- program16.java
+++ program16.java
@@ -1,6 +1,5 @@
 /**
  * Total and Average of 3 marks
- * Verifying the Mark Range 0 to 100 -- Version 1
  */
 import java.util.Scanner;
 public class program16 {
@@ -18,15 +17,12 @@ public class program16 {
 
         input.close();
 
-        if((0 > m1 || 100 < m1) || (0 > m2 || 100 < m2) || (0 > m3 || 100 < m3)) {
-            System.out.print("Mark is out of Range");
-        }
-        else{
-            int total =m1 + m2 + m3;
-            float average = total / 3.0F;
-            
-            System.out.printf("Mark 1 : %d%nMark 2 : %d%nMark 3 : %d%n", m1, m2, m3);
-            System.out.printf("Total : %d%nAverage : %.2f", total, average);
-        }        
+        int total =m1 + m2 + m3;
+
+        float average = total / 3.0F;
+        
+        System.out.printf("Mark 1 : %d%nMark 2 : %d%nMark 3 : %d%n", m1, m2, m3);
+        System.out.printf("Total : %d%nAverage : %.2f", total, average);
+        
     }
 }
