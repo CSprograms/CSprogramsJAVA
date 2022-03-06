abfb801bb96a429e4105357906c5920dd562f4a6
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-06 10:10:54 +0530

    Update program13.java

--- program13.java
+++ program13.java
@@ -4,7 +4,7 @@
 public class program13 {
   public static void main(String[] args){
     char lC = 'a';
-    char uC = (char)(lC - 32);
+    char uC = Character.toUpperCase(lC);
     
     System.out.println("Lowercase : " + lC);
     System.out.print("Uppercase : " + uC);
