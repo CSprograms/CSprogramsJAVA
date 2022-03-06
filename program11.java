a68e5004f22f3553e4b5a41abe845a5fb4d460c4
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-06 11:02:14 +0530

    Update program11.java

--- program11.java
+++ program11.java
@@ -2,9 +2,15 @@
  * Celsius to Fahrenheit
  * Fahrenheit = 32 + ( ( 9 * Celsius ) / 5 )
  */
+import java.util.Scanner;
 public class program11 {
     public static void main(String[] args){
-        int c = 28;
+        Scanner ip = new Scanner(System.in);
+
+        System.out.print("Enter the Celsius : ");
+        int c = ip.nextInt();
+
+        ip.close();
         
         int f = 32 + (( 9 * c)/5);
 
