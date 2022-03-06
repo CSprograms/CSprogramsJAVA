e0fe2419ab6263302a2ecbea166d14c830843d5d
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-06 19:47:58 +0530

    Update program19.java

--- program19.java
+++ program19.java
@@ -1,21 +1,19 @@
 /**
- * Biggest amonng Two Numbers -- Version 1
- * Using Ternary Operator
+ * String Read, Write and Length
  */
 import java.util.Scanner;
 public class program19 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the string : ");
+        String word = input.nextLine();
 
-        System.out.print("Enter Input 1 : ");
-        int input1 = input.nextInt(); 
-
-        System.out.print("Enter Input 2 : ");
-        int input2 = input.nextInt();
         input.close();
 
-        int biggest = input1 > input2 ? input1 : input2;
+        int length = word.length();
 
-        System.out.println("Biggest Value : " + biggest);
-    }
+        System.out.println("Given String : " + word);
+        System.out.println("String Length : " + length);        
+    }    
 }
