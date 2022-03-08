cdb77da9eb41c4399f6b1b06dfc4e567fe57cb13
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-08 08:45:58 +0530

    Update program12.java

--- program12.java
+++ program12.java
@@ -1,11 +1,18 @@
 /**
  * Kilometer to Miles
+ * 1km = 0.6213711922mi
  */
+ import java.util.Scanner; 
 public class program12 {
   public static void main(String[] args){
-    final double MILES_PER_KM = 0.621371;
+	Scanner ip = new Scanner(System.in);  
+    final double MILES_PER_KM = 0.6213711922;
     
-    double kiloMeter = 3.2;
+	System.out.print("Enter the distance in km : ");
+    double kiloMeter = ip.nextInt();
+	
+	ip.close();
+	
     double miles = kiloMeter * MILES_PER_KM;
     
     System.out.println("Kilometer : " + kiloMeter);
