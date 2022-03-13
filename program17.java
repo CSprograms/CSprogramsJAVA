7e9b72996e1aa286e5d552784fa1ccd3b87f0a07
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-13 15:59:40 +0530

    Update program17.java

--- program17.java
+++ program17.java
@@ -10,23 +10,32 @@ public class program17 {
         System.out.print("Enter Mark 1 : ");
         int m1 = input.nextInt();
 
-        System.out.print("Enter Mark 2 : ");
-        int m2 = input.nextInt();
-
-        System.out.print("Enter Mark 3 : ");
-        int m3 = input.nextInt();
-
-        input.close();
-
-        if((0 > m1 || 100 < m1) || (0 > m2 || 100 < m2) || (0 > m3 || 100 < m3)) {
-            System.out.print("Mark is out of Range");
+        if(0 > m1 || 100 < m1) {
+            System.out.print("Mark 1 is out of Range");
         }
-        else{
-            int total =m1 + m2 + m3;
-            float average = total / 3.0F;
-            
-            System.out.printf("Mark 1 : %d%nMark 2 : %d%nMark 3 : %d%n", m1, m2, m3);
-            System.out.printf("Total : %d%nAverage : %.2f", total, average);
-        }        
+        else {
+            System.out.print("Enter Mark 2 : ");
+            int m2 = input.nextInt();
+
+            if(0 > m2 || 100 < m2) {
+                System.out.print("Mark 2 is out of Range");
+            }
+            else {
+                System.out.print("Enter Mark 3 : ");
+                int m3 = input.nextInt();
+
+                if(0 > m3 || 100 < m3) {
+                    System.out.print("Mark 3 is out of Range");
+                }
+                else{
+                    int total =m1 + m2 + m3;
+                    float average = total / 3.0F;
+                    
+                    System.out.printf("Mark 1 : %d%nMark 2 : %d%nMark 3 : %d%n", m1, m2, m3);
+                    System.out.printf("Total : %d%nAverage : %.2f", total, average);
+                }
+            }
+        }
+        input.close();
     }
 }
