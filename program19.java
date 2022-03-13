1e109b051fdbda4eb7ede058ce6d131c5357938f
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-13 15:59:53 +0530

    Update program19.java

--- program19.java
+++ program19.java
@@ -1,19 +1,24 @@
 /**
- * String Read, Write and Length
+ * Reading a String
+ * next() vs nextLine()
  */
 import java.util.Scanner;
 public class program19 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Enter the string : ");
-        String word = input.nextLine();
+        System.out.print("Enter the string 1 : ");
+        String str1 = input.next();
+
+        if(input.hasNext())
+        input.nextLine();
+
+        System.out.print("Enter the string 2 : ");
+        String str2 = input.nextLine();
 
         input.close();
 
-        int length = word.length();
-
-        System.out.println("Given String : " + word);
-        System.out.println("String Length : " + length);        
+        System.out.println("String 1 : " + str1);
+        System.out.print("String 2 : " + str2);        
     }    
 }
