076613b78d40456572385f61fc086f44b36ed62a
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-13 16:00:00 +0530

    Update program20.java

--- program20.java
+++ program20.java
@@ -1,30 +1,19 @@
 /**
- * Swaping -- with temporary variable
+ * Finding the String Length
  */
 import java.util.Scanner;
 public class program20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter Input 1 : ");
-        int input1 = input.nextInt(); 
-
-        System.out.print("Enter Input 2 : ");
-        int input2 = input.nextInt();
         
+        System.out.print("Enter the string : ");
+        String word = input.next();
+
         input.close();
 
-        System.out.println("Before Swap");
-        System.out.println("Input 1 : " + input1);
-        System.out.println("Input 2 : " + input2);
+        int length = word.length();
 
-        int temp = input1;
-        input1 = input2;
-        input2 = temp;
-
-        System.out.println("After Swap");
-        System.out.println("Input 1 : " + input1);
-        System.out.println("Input 2 : " + input2);
-
-    }
+        System.out.println("Given String : " + word);
+        System.out.println("String Length : " + length);        
+    }    
 }
