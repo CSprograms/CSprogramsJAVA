a9d314a44d24901653ca0a788c111a26c127d4d3
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-22 14:00:49 +0530

    Create program32.java

---
+++ program32.java
@@ -0,0 +1,25 @@
+/**
+ * Biggest amonng Two Numbers -- Version 2
+ * Using Simple if
+ */
+import java.util.Scanner;
+public class program32 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Input 1 : ");
+        int input1 = input.nextInt(); 
+
+        System.out.print("Enter Input 2 : ");
+        int input2 = input.nextInt();
+        input.close();
+
+        int biggest = input1;
+        
+        if( input2 > biggest ) {
+            biggest = input2;
+        }
+
+        System.out.println("Biggest Value : " + biggest);
+    }
+}
