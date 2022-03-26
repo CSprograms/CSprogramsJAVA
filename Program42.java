5a33fca08d7999d17cd0be0673b30dabb1733eca
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 18:08:40 +0530

    Create Program42.java

---
+++ Program42.java
@@ -0,0 +1,15 @@
+/**
+ * Sum of two integers - Version 1
+ */
+public class Program42 {
+    public static void main(String[] args) {
+        
+		int ip1 = 6;
+		int ip2 = 24;
+
+        int sum = ip1 + ip2;
+
+        System.out.printf("%d + %d = %d", ip1, ip2, sum);
+    }
+
+}
