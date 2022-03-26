f8d9a6419b24938caa1df995e88a41b19bcc708c
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-26 18:08:46 +0530

    Create Program43.java

---
+++ Program43.java
@@ -0,0 +1,18 @@
+/**
+ * Sum of two integers using data members - Version 2
+ */
+public class Program43 {
+	
+    private static int ip1, ip2, sum;
+	
+    public static void main(String[] args) {
+        
+		ip1 = 6;
+		ip2 = 24;
+
+        sum = ip1 + ip2;
+
+        System.out.printf("%d + %d = %d", ip1, ip2, sum);
+    }
+
+}
