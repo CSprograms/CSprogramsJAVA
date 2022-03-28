b77ff98d04771f225fdd7c3757f0ffabca9ed7c5
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-28 13:44:16 +0530

    Update Program43.java

--- Program43.java
+++ Program43.java
@@ -3,14 +3,15 @@
  */
 public class Program43 {
 	
-    private static int ip1, ip2, sum;
+	private static int ip1;
+	private static int ip2;
+	private static int sum;
 	
     public static void main(String[] args) {
         
 		ip1 = 6;
 		ip2 = 24;
-
-        sum = ip1 + ip2;
+		sum = ip1 + ip2;
 
         System.out.printf("%d + %d = %d", ip1, ip2, sum);
     }
