a8c7e8ef042b2cde66f8d30e442545c92d6419d1
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-28 16:11:53 +0530

    Update Program45.java

--- Program45.java
+++ Program45.java
@@ -4,31 +4,33 @@
  */
 public class Program45 {
 	
-    private int ip1, ip2, sum;
+	private int ip1;
+	private int ip2;
+	private int sum;
 	
-    public void set(int ip1, int ip2) {
-        this.ip1 = ip1;
-        this.ip2 = ip2;
-    }
+	public void set(int ip1, int ip2) {
+		this.ip1 = ip1;
+		this.ip2 = ip2;
+	}
 	
-    public int get_ip1() {
-        return this.ip1;
-    }
+	public int get_ip1() {
+		return this.ip1;
+	}
 	
-    public int get_ip2() {
-        return this.ip2;
-    }
+	public int get_ip2() {
+		return this.ip2;
+	}
 	
-    public int get_sum() {
-        this.sum = this.ip1 + this.ip2;
-        return sum;
-    }
+	public int get_sum() {
+		this.sum = this.ip1 + this.ip2;
+		return this.sum;
+	}
 	
     public static void main(String[] args) {
 		
-        Program45 obj = new Program45();
-
-        obj.set(6, 24);
+		Program45 obj = new Program45();
+        
+		obj.set(6, 24);
 
         System.out.printf("%d + %d = %d", obj.get_ip1(), obj.get_ip2(), obj.get_sum());
     }
