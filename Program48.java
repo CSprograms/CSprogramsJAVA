ecce983867c9f56d5d84e442e8f93c02c45df86e
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-28 16:12:21 +0530

    Update Program48.java

--- Program48.java
+++ Program48.java
@@ -4,59 +4,63 @@
  */
 class Addition2 {
 	
-    private int ip1, ip2, sum;
+	private int ip1;
+	private int ip2;
+	private int sum;
 	
-    public Addition2() {
-        this.ip1 = 0;
-        this.ip2 = 0;
-    }
+	Addition2() {
+		this.ip1 = 0;
+		this.ip2 = 0;
+	}
 	
-    public Addition2(int ip) {
-        this.ip1 = ip;
-        this.ip2 = ip;
-    }
+	Addition2(int ip) {
+		this.ip1 = ip;
+		this.ip2 = ip;
+	}
 	
-    public Addition2(int ip1, int ip2) {
-        this.ip1 = ip1;
-        this.ip2 = ip2;
-    }
+	Addition2(int ip1, int ip2) {
+		this.ip1 = ip1;
+		this.ip2 = ip2;
+	}
 	
-    public void set(int ip1, int ip2) {
-        this.ip1 = ip1;
-        this.ip2 = ip2;
-    }
+	public void set(int x, int y) {
+		this.ip1 = x;
+		this.ip2 = y;
+	}
 	
-    public int get_ip1() {
-        return this.ip1;
-    }
+	public int get_ip1() {
+		return this.ip1;
+	}
 	
-    public int get_ip2() {
-        return this.ip2;
-    }
+	public int get_ip2() {
+		return this.ip2;
+	}
 	
-    public int get_sum() {
-        sum = ip1 + ip2;
-        return sum;
-    }
+	public int get_sum() {
+		this.sum = this.ip1 + this.ip2;
+		return this.sum;
+	}
 	
-    public String toString() {
-        return this.ip1 + " + " + this.ip2 + " = " + this.get_sum();
-    }
+	public String toString() {
+		return this.ip1 + " + " + this.ip2 + " = " + this.get_sum();
+	}
+
 }
-public class Program48 {        
-    public static void main(String[] args) {
-
-        Addition2 obj1 = new Addition2();
+public class Program48 {
+	
+	public static void main(String[] args) {
+		
+		Addition2 obj1 = new Addition2();
         System.out.println(obj1.toString());
-
-        Addition2 obj2 = new Addition2(6);
+		
+		Addition2 obj2 = new Addition2(6);
         System.out.println(obj2.toString());
-
-        Addition2 obj3 = new Addition2(23, 24);
+		
+		Addition2 obj3 = new Addition2(6, 24);
         System.out.println(obj3.toString());
-
-        obj3.set(10, 20);
-        System.out.print(obj3.toString());
+		
+		obj3.set(10, 20);
+		System.out.print(obj3.toString());
     }
 
 }
