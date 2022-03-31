236f923c5037e49b2c6fb62bde3b8aecfa171efc
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:07:50 +0530

    Create program08.java

---
+++ basicPrograms/program08.java
@@ -0,0 +1,16 @@
+/**
+* String concatenation VS Addition
+*/
+package basicPrograms;
+public class program08
+{
+   public static void main(String[] args) 
+   {
+	  int var1 = 5;
+	  int var2 = 3;
+      
+     System.out.println("String Concatenation: " + var1 + var2);
+     
+	  System.out.print("Addition: " + (var1 + var2));
+   } 
+} 
