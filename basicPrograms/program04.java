5372fb2af68b08bccdc5ef11bb45587555b8bf75
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:07:18 +0530

    Create program04.java

---
+++ basicPrograms/program04.java
@@ -0,0 +1,14 @@
+/**
+* println vs print method
+*/
+package basicPrograms;
+public class program04 {
+   public static void main(String[] args) {
+
+      System.out.println("Welcome to");
+      System.out.println(" Java Programming!");
+      
+      System.out.print("Welcome to");
+      System.out.print(" Java Programming!");
+   } 
+} 
