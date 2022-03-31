fd397cea12c7689dc94c9a04dd6a5720d60fb19a
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:08:10 +0530

    Create program10.java

---
+++ basicPrograms/program10.java
@@ -0,0 +1,24 @@
+/**
+ * Arithmetic Operations on double
+ */
+package basicPrograms;
+import java.util.Scanner;
+public class program10 {
+  public static void main(String[] args) {
+    Scanner ip = new Scanner(System.in);
+    
+    System.out.print("Enter Input 1 : ");
+    double ip1 = ip.nextDouble();
+    
+    System.out.print("Enter Input 2 : ");
+    double ip2 = ip.nextDouble();
+    
+    ip.close();
+    
+    System.out.printf("%.4f + %.4f = %.4f%n", ip1, ip2, (ip1+ip2));
+    System.out.printf("%.4f - %.4f = %.4f%n", ip1, ip2, (ip1-ip2));
+    System.out.printf("%.4f * %.4f = %.4f%n", ip1, ip2, (ip1*ip2));
+    System.out.printf("%.4f / %.4f = %.4f%n", ip1, ip2, (ip1/ip2));
+    System.out.printf("%.4f %s %.4f = %.4f%n", ip1, "%", ip2, (ip1%ip2));
+  }
+}
