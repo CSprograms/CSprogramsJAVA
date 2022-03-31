71f06add3203ed41d68a2f85495474caca4e25a1
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:06:47 +0530

    Create program01.java

---
+++ basicPrograms/program01.java
@@ -0,0 +1,11 @@
+/**
+ * Printing a string using println method
+ */
+package basicPrograms;
+
+public class program01 {
+   public static void main(String[] args) {
+
+      System.out.println("Welcome to Java Programming!");
+   } 
+} 
