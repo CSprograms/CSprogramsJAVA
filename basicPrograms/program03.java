8f9a2c61ca2af3f095626aed13e4aaac3699a203
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:07:09 +0530

    Create program03.java

---
+++ basicPrograms/program03.java
@@ -0,0 +1,12 @@
+/**
+* Printing multi-line string using multiple println method
+*/
+package basicPrograms;
+
+public class program03 {
+   public static void main(String[] args) {
+      
+      System.out.println("Welcome to");
+      System.out.println(" Java Programming!");
+   } 
+} 
