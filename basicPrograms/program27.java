9784873c122c9c63d676b37c0fa2f5d18f7ec92f
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:09:56 +0530

    Create program27.java

---
+++ basicPrograms/program27.java
@@ -0,0 +1,22 @@
+/**
+ * Character to ASCII
+ */
+package basicPrograms;
+import java.util.Scanner;
+public class program27{
+    public static void main(String[] args) {
+		System.out.println("Character to ASCII");
+		System.out.println("******************");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a Character : ");
+        char ch = input.next().charAt(0);
+
+        input.close();
+
+        int ASCII = ch;     // Implicit type conversion from char to int
+
+        System.out.println("Given Character : " + ch);
+        System.out.print("ASCII Value : " + ASCII);
+    }
+}
