28d4288fefff780dcea93880e70e5e06e62a2dd5
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:07:35 +0530

    Create program06.java

---
+++ basicPrograms/program06.java
@@ -0,0 +1,12 @@
+/**
+* String formatting in printf method
+*/
+package basicPrograms;
+public class program06 {
+   public static void main(String[] args) {
+
+      System.out.printf("%s%n","Welcome to Java Programming!");
+      
+      System.out.printf("%s\n%s","Welcome to " , "Java Programming!");
+   } 
+} 
