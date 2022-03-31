1927413321a6557070e0c3d2097af47879f36a5a
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:07:00 +0530

    Create program02.java

---
+++ basicPrograms/program02.java
@@ -0,0 +1,10 @@
+/**
+* Printing multi-line string using println method
+*/
+package basicPrograms;
+public class program02 {
+   public static void main(String[] args) {
+      
+      System.out.println("Welcome to\n Java Programming!");
+   } 
+} 
