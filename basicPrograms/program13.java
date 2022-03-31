c217cffa45d494ec1aec55d8b68eb55152026595
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:08:37 +0530

    Create program13.java

---
+++ basicPrograms/program13.java
@@ -0,0 +1,13 @@
+/**
+ * Lowercase to Uppercase -- Version 1
+ */
+package basicPrograms;
+public class program13 {
+  public static void main(String[] args){
+    char lC = 'a';
+    char uC = (char) (lC - 32);
+    
+    System.out.println("Lowercase : " + lC);
+    System.out.println("Uppercase : " + uC);  
+  }
+}
