5fc96be622cb484d5b7483fbadfc2dbe7e35c2f2
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:09:47 +0530

    Create program26.java

---
+++ basicPrograms/program26.java
@@ -0,0 +1,22 @@
+/**
+ * Degree to Radian
+ */
+package basicPrograms;
+import java.util.Scanner;
+public class program26{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Degree Value : ");
+        int degree = input.nextInt(); 
+
+        input.close();
+
+		double radian = degree * Math.PI /180;
+
+		System.out.println("Degree Value : " + degree);
+        System.out.println("Radian Value (Manual Calculation) : " + radian);
+        System.out.print("Radian Value (Buil-in Method) : " + Math.toRadians(degree));
+		
+    }
+}
