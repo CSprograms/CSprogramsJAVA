d871e6cc51324f5c64b4a8ae96cccf9369d78cee
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-03-31 16:08:59 +0530

    Create program15.java

---
+++ basicPrograms/program15.java
@@ -0,0 +1,27 @@
+/**
+ * Body Mass Index (BMI) = Weight / Height^2
+ * Weight in KiloGram
+ * Height in Meter
+ */
+package basicPrograms;
+import java.util.Scanner;
+public class program15 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        final double METER_PER_FOOT = 0.3046;
+
+        System.out.print("Enter the Weight : ");
+        double weight = ip.nextDouble();
+
+        System.out.print("Enter the Height : ");
+        double heightInFeet = ip.nextDouble();
+
+        ip.close();
+
+        double height = heightInFeet * METER_PER_FOOT;
+
+        System.out.println("Weight : " + weight);
+        System.out.println("Height : " + height);
+        System.out.println("BMI : " + Math.round(weight / Math.pow(height,2) ) );
+    }
+}
