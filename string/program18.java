f037173e155aeb94d7d74a7216aa147e2b9a2c04
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-04-07 13:20:58 +0530

    Update program18.java

--- string/program18.java
+++ string/program18.java
@@ -9,13 +9,10 @@ public class program18 {
         Scanner input = new Scanner(System.in);
         
         System.out.print("Enter the string 1 : ");
-        String str1 = input.next();
-
-        if(input.hasNext())
-        input.nextLine();
+        String str1 = input.nextLine();
 
         System.out.print("Enter the string 2 : ");
-        String str2 = input.nextLine();
+        String str2 = input.next();
 
         input.close();
 
