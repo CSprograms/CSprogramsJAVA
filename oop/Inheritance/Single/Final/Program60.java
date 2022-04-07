1ce4cd9296d0d5f2eb97b7c1fec9d601830a776c
CSprograms <aravindhanmohan.tvm@gmail.com>
2022-04-07 13:05:39 +0530

    Create Program60.java

---
+++ oop/Inheritance/Single/Final/Program60.java
@@ -0,0 +1,34 @@
+/**
+ * Single Inheritance - Final Class - Compilation Error
+ */
+//package oop.Inheritance.Single.Final;
+
+final class Parent {
+
+    public void display() {
+        System.out.println("display() in Parent Class.");
+    }
+}
+
+// error: cannot inherit from final Parent
+class Child extends Parent {
+    
+	public void display() {
+        System.out.println("display() in Child Class.");
+    }
+}
+public class Program60 {
+    public static void main(String[] args) {
+
+        Parent obj1 = new Parent();
+        Child obj2 = new Child();
+
+        Parent obj = obj1;
+        System.out.println("Calling display() from parent class reference.");
+        obj.display();
+
+        obj = obj2;
+        System.out.println("Calling display() from child class reference.");
+        obj.display();
+    }    
+}
